b9a197de3d5bf938c3de2f4ad39051b05aafb6bf
jaymac00 <jmm0191@auburn.edu>
2025-11-17 12:25:05 -0600

    format buttons and center everything

--- ti4reference/functions.js
+++ ti4reference/functions.js
@@ -6,8 +6,8 @@ export function populatePromissories() {
 	document.getElementById("content").innerHTML = "";
 	for (const promissory in promissories){
 		var txt = "";
-		txt += "<h3>" + promissories[promissory]["name"] + "</h3>";
-		txt += "<p>" + promissories[promissory]["desc"] + "</p>";
+		txt += "<h3 class=\"promissoryHeader\">" + promissories[promissory]["name"] + "</h3>";
+		txt += "<p class=\"promissoryBody\">" + promissories[promissory]["desc"] + "</p>";
 		document.getElementById("content").innerHTML += txt;
 	}
 
@@ -20,11 +20,11 @@ export function populateStrategies() {
 	document.getElementById("content").innerHTML = "";
 	for (const strategy in strategies){
 		var txt = "";
-		txt += "<h3>" + strategies[strategy]["priority"] + " - " + strategies[strategy]["name"] + "</h3>";
-		txt += "<h4>Primary</h4>";
-		txt += "<p>" + strategies[strategy]["primary"] + "</p>";
-		txt += "<h4>Secondary</h4>";
-		txt += "<p>" + strategies[strategy]["secondary"] + "</p>";
+		txt += "<h3 class=\"strategyHeader\">" + strategies[strategy]["priority"] + " - " + strategies[strategy]["name"] + "</h3>";
+		txt += "<h4 class=\"primaryHeader\">Primary</h4>";
+		txt += "<p class=\"primaryBody\">" + strategies[strategy]["primary"] + "</p>";
+		txt += "<h4 class=\"secondaryHeader\">Secondary</h4>";
+		txt += "<p class=\"secondaryBody\">" + strategies[strategy]["secondary"] + "</p>";
 		document.getElementById("content").innerHTML += txt;
 	}
 
@@ -37,15 +37,15 @@ export function populateTechnologies() {
 	document.getElementById("content").innerHTML = "";
 	for (const type in technologies){
 		var txt = "";
-		txt += "<h3>" + technologies[type]["type"] + "</h3>";
+		txt += "<h3 class=\"technologyHeader\">" + technologies[type]["type"] + "</h3>";
 		for (const technology in technologies[type]){
 			if (technology != "type"){
 				if (technologies[type][technology]["prereq"] != "") {
-					txt += "<h4>" + technologies[type][technology]["name"] + " (Prerequisite: " + technologies[type][technology]["prereq"] + ")</h4>";
+					txt += "<h4 class=\"technologyName\">" + technologies[type][technology]["name"] + " (Prerequisite: " + technologies[type][technology]["prereq"] + ")</h4>";
 				} else {
-					txt += "<h4>" + technologies[type][technology]["name"] + "</h4>";
+					txt += "<h4 class=\"technologyName\">" + technologies[type][technology]["name"] + "</h4>";
 				}
-				txt += "<p>" + technologies[type][technology]["desc"] + "</p>";
+				txt += "<p class=\"technologyBody\">" + technologies[type][technology]["desc"] + "</p>";
 			}
 		}
 		document.getElementById("content").innerHTML += txt;
@@ -60,27 +60,27 @@ export function populateUnits() {
 	document.getElementById("content").innerHTML = "";
 	for (const unit in units){
 		var txt = "";
-		txt += "<h3>" + units[unit]["name"] + " (" + units[unit]["type"] + ")</h3>";
+		txt += "<h3 class=\"unitHeader\">" + units[unit]["name"] + " (" + units[unit]["type"] + ")</h3>";
 		if (units[unit]["abilities"].length > 0) {
 			for (const ability in units[unit]["abilities"]) {
-				txt += "<p>" + units[unit]["abilities"][ability] + "</p>";
+				txt += "<p class=\"unitAbilities\">" + units[unit]["abilities"][ability] + "</p>";
 			}
 		}
-		txt += "<p>cost: " + units[unit]["cost"] + " | combat: " + units[unit]["combat"] + " | move: " + units[unit]["move"] + " | capacity: " + units[unit]["capacity"] + "</p>";
+		txt += "<p class=\"unitBody\">cost: " + units[unit]["cost"] + " | combat: " + units[unit]["combat"] + " | move: " + units[unit]["move"] + " | capacity: " + units[unit]["capacity"] + "</p>";
 		if (unit != "mech" && unit != "warSun") {
-			txt += "<p><i>Upgrade to <b>" + units[unit]["upgrade"]["name"] + "</b>:</i></p>";
+			txt += "<p class=\"unitUpgrade\"><i>Upgrade to <b>" + units[unit]["upgrade"]["name"] + "</b>:</i></p>";
 			if (units[unit]["upgrade"]["abilities"].length > 0) {
 				for (const ability in units[unit]["upgrade"]["abilities"]) {
-					txt += "<p>" + units[unit]["upgrade"]["abilities"][ability] + "</p>";
+					txt += "<p class=\"unitUpgradeAbilities\">" + units[unit]["upgrade"]["abilities"][ability] + "</p>";
 				}
 			}
-			txt += "<p>cost: " + units[unit]["upgrade"]["cost"] + " | combat: " + units[unit]["upgrade"]["combat"] + " | move: " + units[unit]["upgrade"]["move"] + " | capacity: " + units[unit]["upgrade"]["capacity"] + "</p>";
+			txt += "<p class=\"unitUpgradeBody\">cost: " + units[unit]["upgrade"]["cost"] + " | combat: " + units[unit]["upgrade"]["combat"] + " | move: " + units[unit]["upgrade"]["move"] + " | capacity: " + units[unit]["upgrade"]["capacity"] + "</p>";
 		} else if (unit == "warSun") {
-			txt += "<p><i>Must research this unit's upgrade technology to produce this unit:</i></p>";
+			txt += "<p class=\"unitUpgrade\"><i>Must research this unit's upgrade technology to produce this unit:</i></p>";
 			for (const ability in units[unit]["upgrade"]["abilities"]) {
-				txt += "<p>" + units[unit]["upgrade"]["abilities"][ability] + "</p>";
+				txt += "<p class=\"unitUpgradeAbilities\">" + units[unit]["upgrade"]["abilities"][ability] + "</p>";
 			}
-			txt += "<p>cost: " + units[unit]["upgrade"]["cost"] + " | combat: " + units[unit]["upgrade"]["combat"] + " | move: " + units[unit]["upgrade"]["move"] + " | capacity: " + units[unit]["upgrade"]["capacity"] + "</p>";
+			txt += "<p class=\"unitUpgradeBody\">cost: " + units[unit]["upgrade"]["cost"] + " | combat: " + units[unit]["upgrade"]["combat"] + " | move: " + units[unit]["upgrade"]["move"] + " | capacity: " + units[unit]["upgrade"]["capacity"] + "</p>";
 		}
 		document.getElementById("content").innerHTML += txt;
 	}
